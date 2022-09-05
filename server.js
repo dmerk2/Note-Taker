f1ae4a52ab595a4ac05da0d5ba16b4d541f57631
Daniel Merkin <dan.merkin@gmail.com>
2022-09-05 13:36:43 -0400

    adding middleware and requiring db

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require("express");
 const api = require("./routes/notes.js");
 const uuid = require("uuid");
 const app = express("express");
-const fs = require('fs');
-
+const database = require('./Develop/db/db.json')
+let path = require("path")
 const PORT = process.env.port || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+// app.use(express.static('public'));
+
 // GET Route for homepage
-app.get("/", (req, res) =>
+app.get("/", (req, res) =>{
+let newNote = req.body;
   res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+  database.push(newNote);
+});
 
 // GET Route for notes page
 app.get("/notes", (req, res) =>
