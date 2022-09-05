02ada4503b1ff4b60b46047103240c66db1285e8
Daniel Merkin <dan.merkin@gmail.com>
2022-09-05 13:59:36 -0400

    creating a notesRouter

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,10 @@
-const homepage = require("express").Router();
-const { v4: uuidv4 } = require("uuid");
+const express = require("express");
 
-homepage.post("/", (req, res) => {
-    const { isValid, errors } = req.body;
+// Import the modular routers for /notes
+const notesRouter = require("./notes");
 
-    const newTask = {
-        time: Date.now(),
-        error_id: uuidv4(),
-        errors,
-    }
+const app = express();
 
-    if(!isValid) {
-        readAndAppend(newTask, './Develop/db/db.json');
-        res.json('New Task Added');
-    } else {
-        res.json({
-            message: 'Object is valid but not logging.',
-            error_id: newTask.error_id,
-        })
-    }
-});
+app.use('/notes', notesRouter);
 
-module.exports = homepage;
+module.exports = app;
